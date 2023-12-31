49b217f45372dcc6c82bee9038a4c00ac44aedb3
Amirkhan <torexanovich.l@gmail.com>
2023-12-31 14:12:21 +0500

    add charge card

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -15,33 +15,33 @@ const cssVersion = "1"
 
 type config struct {
 	port int
-	env string
-	api string
-	db struct {
+	env  string
+	api  string
+	db   struct {
 		dsn string
 	}
 	stripe struct {
 		secret string
-		key string
+		key    string
 	}
 }
 
 type application struct {
-	config config
-	infoLog *log.Logger
-	errorLog *log.Logger
+	config        config
+	infoLog       *log.Logger
+	errorLog      *log.Logger
 	templateCache map[string]*template.Template
-	version string
+	version       string
 }
 
 func (app *application) serve() error {
-	srv := &http.Server {
-		Addr: fmt.Sprintf(":%d", app.config.port),
-		Handler: app.routes(),
-		IdleTimeout: 30 * time.Second,
-		ReadTimeout: 10 * time.Second,
+	srv := &http.Server{
+		Addr:              fmt.Sprintf(":%d", app.config.port),
+		Handler:           app.routes(),
+		IdleTimeout:       30 * time.Second,
+		ReadTimeout:       10 * time.Second,
 		ReadHeaderTimeout: 5 * time.Second,
-		WriteTimeout: 5 * time.Second,
+		WriteTimeout:      5 * time.Second,
 	}
 
 	app.infoLog.Printf("Starting HTTP server in %s mode on port %d", app.config.env, app.config.port)
@@ -67,11 +67,11 @@ func main() {
 	tc := make(map[string]*template.Template)
 
 	app := &application{
-		config: cfg,
-		infoLog: infoLog,
-		errorLog: errorLog,
+		config:        cfg,
+		infoLog:       infoLog,
+		errorLog:      errorLog,
 		templateCache: tc,
-		version: version,
+		version:       version,
 	}
 
 	err := app.serve()
